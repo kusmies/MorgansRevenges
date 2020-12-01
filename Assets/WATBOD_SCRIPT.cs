b8dcddcb089e0f11c6fad82d489613e3bf5cf221
tBlueMage <47580410+tBlueMage@users.noreply.github.com>
2020-12-01 11:21:43 -0500

    Update 12/1/20 a

--- Assets/WATBOD_SCRIPT.cs
+++ Assets/WATBOD_SCRIPT.cs
@@ -7,6 +7,7 @@ public class WATBOD_SCRIPT : MonoBehaviour
     public int rowLength;
     public int columnHeight;
     public bool isFrozen = false;
+    bool alreadyFroze = false;
     public GameObject childExample1; // Subsurface water
     public GameObject childExample2; // Surface water. Spawn this .7 units above the final row
     public GameObject[,] tileChildren1; //The 2D array that holds all of the subsurface water.
@@ -18,8 +19,9 @@ public class WATBOD_SCRIPT : MonoBehaviour
     {
         myTran = GetComponent<Transform>(); //Get the transform of the parent.
         tileChildren1 = new GameObject[columnHeight, rowLength]; //Make a 2d array based on the column height and row length.
+        tileChildren2 = new GameObject[rowLength];
 
-        for (int i = 0; i < columnHeight; i++) //A weird for loop that makes all of the tile children. We iterate for every point in the column.
+        for (int i = 0; i < columnHeight; i++) //A weird for loop that makes all of the subsurface water. We iterate for every point in the column.
         {
             for (int j = 0; j < rowLength; j++) //And for every point in the column we iterate for every point in the row.
             {
@@ -36,11 +38,26 @@ public class WATBOD_SCRIPT : MonoBehaviour
                 tileChildren1[i, j] = newChild; //And finally, we add the child to the 2d array based on i and j.
             }
         }
+
+        for (int k = 0; k < rowLength; k++) //A more normal loop that makes the surface water. Looks like gentle white waves in game.
+        {
+            Vector3 childSpawn = new Vector3(k, columnHeight - .3f, 0);
+
+            childSpawn = (childSpawn + myTran.transform.position); //One final adjustment to the spawn location based on the actual location of the parent.
+
+            GameObject newChild = Instantiate(childExample2, childSpawn, myTran.rotation); //Now we instantiate a child. If only it were that easy.
+
+            WATSURF_SCRIPT childScript = newChild.GetComponent<WATSURF_SCRIPT>(); //Before we let the child go off on his own we need to store that child's script into a temporary variable.
+
+            childScript.parent = GetComponent<WATBOD_SCRIPT>(); //We also remind that child who was the parent that birthed it.
+
+            tileChildren2[k] = newChild; //And finally, we add the child to the 2d array based on i and j.
+        }
     }
 
     void Update()
     {
-        if (isFrozen) 
+        if (isFrozen && !alreadyFroze) 
         {
             for (int i = 0; i < columnHeight; i++) //For every point of the column height
             {
@@ -53,6 +70,13 @@ public class WATBOD_SCRIPT : MonoBehaviour
                 }
             }
             
+            for (int k = 0; k < tileChildren2.Length; k++)
+            {
+                var childScript = tileChildren2[k].GetComponent<WATSURF_SCRIPT>();
+
+                childScript.isDead = true;
+            }
+            alreadyFroze = true;
         }
     }
 
--- Assets/WATSURF_SCRIPT.cs
+++ Assets/WATSURF_SCRIPT.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WATSURF_SCRIPT : MonoBehaviour
 {
+    public WATBOD_SCRIPT parent;
+
+    public bool isDead = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDead)
+        {
+            Destroy(gameObject);
+        }
     }
 }
